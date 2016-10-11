a2c6a608fe68170c0c8549ebff48863ee5e7ed44
j201 <j201.alex@gmail.com>
2016-10-10 22:47:29 -0400

    Add basic web GUI (you can play the game now!)

--- Main.elm
+++ Main.elm
@@ -1,8 +1,38 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import List exposing (..)
+import Matrix
+import String
 
 import Lexicon exposing (..)
 import Board exposing (..)
 
-main = text ""
+type alias Model = { board: Board, typed: String, scored: List String }
+
+initial : Model
+initial = { board = boardBySeed 0, typed = "", scored = [] }
+
+boardView : Model -> Html msg
+boardView m =
+    table [] (map (\r ->
+        tr [] (map (\c -> td [] [text c]) r))
+                  (Matrix.toList m.board))
+
+update : String -> Model -> Model
+update s m = if String.length s >= 3 && not (member s m.scored) && hasWord s lexicon && wordOnBoard s m.board
+               then { m | typed = "", scored = s::m.scored }
+               else { m | typed = s }
+
+view : Model -> Html String
+view m =
+    div [] [
+        boardView m,
+        input [value m.typed, onInput identity] [],
+        ul [] (map (\s -> li [] [text s]) m.scored)
+    ]
+
+main = App.beginnerProgram { model = initial, update = update, view = view }
